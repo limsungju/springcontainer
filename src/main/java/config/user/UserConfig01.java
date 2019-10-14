f9abf384efd34e4e583b99ae7e9cdd4837d0cade
limsungju <fullmoon4860@gmail.com>
2019-10-14 11:55:55 +0900

    JUnit, Spring TestContext 사용법 #1

--- src/main/java/config/user/UserConfig01.java
+++ src/main/java/config/user/UserConfig01.java
@@ -2,6 +2,7 @@ package config.user;
 
 import org.springframework.context.annotation.Bean;
 
+import kr.co.itcen.springcontainer.user.Friend;
 import kr.co.itcen.springcontainer.user.User;
 
 public class UserConfig01 {
@@ -9,6 +10,11 @@ public class UserConfig01 {
 	@Bean
 	public User user() {
 		return new User(3L, "또치");
+	}
+	
+	@Bean
+	public Friend firend() {
+		return new Friend("둘리");
 	}
 	
 //	@Bean
---
+++ src/test/java/kr/co/itcen/springcontainer/user/UserTest.java
@@ -0,0 +1,40 @@
+package kr.co.itcen.springcontainer.user;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import config.user.UserConfig01;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes= {UserConfig01.class})
+public class UserTest {
+	
+	@Autowired
+	private User user;
+	
+	@Autowired
+	private Friend friend;
+	
+	// Test는 Thread에 의해 병렬로 처리된다.
+	@Test
+	public void testUserNull() {
+		assertNotNull(user); // assert(user != null); 와 동일한 코드
+	}
+	
+	@Test
+	public void testFriendNull() {
+		assertNotNull(friend); //
+	}
+	
+	@Test
+	public void testFriendDooly() {
+		assertEquals(friend.getName(), "둘리");
+	}
+}
