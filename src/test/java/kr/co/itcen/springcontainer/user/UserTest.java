a6d319dfc766f5445ade70564720e7714b2f3cb7
limsungju <fullmoon4860@gmail.com>
2019-10-14 13:15:24 +0900

    JUnit 사용 예시

--- src/test/java/kr/co/itcen/springcontainer/user/UserTest.java
+++ src/test/java/kr/co/itcen/springcontainer/user/UserTest.java
@@ -2,7 +2,6 @@ package kr.co.itcen.springcontainer.user;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
