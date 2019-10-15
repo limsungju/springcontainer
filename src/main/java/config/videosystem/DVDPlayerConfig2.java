b86a0fed518027453d73fb6a93b68df9a69014e1
limsungju <fullmoon4860@gmail.com>
2019-10-15 14:44:44 +0900

    commit

--- src/main/java/config/videosystem/DVDPlayerConfig2.java
+++ src/main/java/config/videosystem/DVDPlayerConfig2.java
@@ -3,7 +3,6 @@ package config.videosystem;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 import kr.co.itcen.springcontainer.videosystem.DVDPlayer;
 import kr.co.itcen.springcontainer.videosystem.DigitalVideoDisc;
